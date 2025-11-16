a453b0061a1c22a6e9664b68545d17d9f65eb6ef
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-16 15:40:05 +0600

    bultin string

--- string.cpp
+++ string.cpp
@@ -1,14 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    int x;
+    // int x;
+    // cin >> x;
+    // cin.ignore();
+    // char s[100];
+    // // cin >> s;
+    // //fgets(s,100,stdin);
+    // cin.getline(s,100);
+    // cout << x << endl<< s<< endl;
+    string x;
     cin >> x;
-    cin.ignore();
-    char s[100];
-    // cin >> s;
-    //fgets(s,100,stdin);
-    cin.getline(s,100);
-    cout << x << endl<< s<< endl;
+    cout << x;
     return 0;
 }
