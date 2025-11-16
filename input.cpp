34efb58cc06163937e7d39519d56963991438d48
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-16 10:18:35 +0600

    input taking

---
+++ input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    double d;
+    cin >> x >> d; 
+    cout<< x << " " << " " << d << endl;
+    char c = 'A';
+    int y = 65;
+    cout << (char) y << endl;
+    return 0;
+}
