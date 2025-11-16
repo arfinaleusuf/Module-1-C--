1cc0a5b5dee7798e15002206a1266cc7f8aa2267
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-16 10:19:36 +0600

    if else in c++

---
+++ if_else.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x = 9;
+    if(x%2== 0)
+    {
+        cout << "even"; 
+    }
+    else
+    {
+        cout << "odd";
+    }
+    return 0;
+}
