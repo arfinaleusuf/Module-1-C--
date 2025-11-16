9052b180c15a7aa459cefb99ded66d32c0ff89d9
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-16 10:19:02 +0600

    printing

---
+++ print.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main()
+{
+    int x = 100;
+    char c = 'A';
+    double d = 34.26;
+
+    std :: cout << x << " " << c << std :: endl << d << std:: endl ;
+    // My favarite number is 100
+    std :: cout << "my favarite number is " << x << std:: endl; 
+    return 0;
+}
